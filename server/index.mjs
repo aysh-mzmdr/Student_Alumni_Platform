65bf9e679647b0eec621a61f3acfbbe7a8c64703
aysh-mzmdr <ayush.mazumdar111@gmail.com>
2025-05-05 22:18:13 +0530

    Created a basic authentication system

--- server/index.mjs
+++ server/index.mjs
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+import passport, { Passport } from "passport"
 
 dotenv.config({path:"../.env"})
 
@@ -9,13 +10,25 @@ const app=express()
 const CLIENT_PORT=process.env.CLIENT_PORT;
 
 app.use(express.json())
+app.use(session())
+app.use(Passport.initialize())
+app.use(Passport.session())
 
 app.use(cors({origin:`http://localhost:${CLIENT_PORT}`}))
 
 const SERVER_PORT=process.env.SERVER_PORT;
 
-app.get("/api/message",(request,response) => {
-    response.json({message:"Ravi Shankar Banerjee"})
+app.post("/auth/login",passport.authenticate("local",{
+    successRedirect:"/dashboard",
+    failureRedirect:"/login"
+}))
+
+app.post("/auth/logout",(request,response) => {
+    if(!request.user) return response.sendStatus(401);
+    request.logout((err) => {
+        if(err) return response.sendStatus(400)
+        return response.sendStatus(200);
+    })
 })
 
 app.listen(SERVER_PORT)
---
+++ server/strategies.js
@@ -0,0 +1,39 @@
+import passport from "passport";
+import { Strategy } from "passport-local";
+
+abcUsers=[
+    {id:0,username:"Ravi",password:"1234"},
+    {id:1,username:"Prem",password:"7890"},
+    {id:2,username:"Mohan",password:"qwerty"},
+    {id:3,username:"Animesh",password:"abcd"},
+    {id:4,username:"Yash",password:"password123"}
+]
+
+passport.serializeUser((user,done) => {
+    done(null,user.id)
+})
+
+passport.deserializeUser((id,done) => {
+    try{
+        const findUser=abcUsers.find(user => user.id==id)
+        if(!findUser) throw new Error("User Not found");
+        done(null,findUser);
+    }
+    catch(err){
+        done(err,null)
+    }
+})
+
+export default passport.use(
+    new Strategy((username,password,done) => {
+        try{
+            const findUser=abcUsers.find((user) => user.username===username);
+            if(!findUser) throw new Error("Error");
+            if(user.password!==password) throw new Error("Error");
+            done(null,findUser);
+        }
+        catch(err){
+            done(err,null)
+        }
+    })
+)
