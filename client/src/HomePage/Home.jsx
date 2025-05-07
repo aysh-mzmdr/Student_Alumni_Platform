0549e86ec12fefd9b03979de25d5e301cc9ec586
Ashish-Kr-1 <ashishkumar.ab01@gmail.com>
2025-05-07 16:55:16 +0530

    Added navbar

--- client/src/HomePage/Home.jsx
+++ client/src/HomePage/Home.jsx
@@ -1,11 +1,31 @@
-import style from "./stylesheet.module.css"
+import styles from "./stylesheet.module.css";
+import { useEffect } from "react";
 
-function Home(){
+function Home() {
 
-    return(
-        <>
-            <h1 className={style.head}>This is Home</h1>
-        </>
+    useEffect(() => {
+        document.body.style.backgroundColor = "rgb(0, 0, 0)";
+
+        return () => {
+            document.body.style.backgroundColor = "";
+        }
+    }, {});
+
+    return (
+        <nav className={styles.navbar}>
+            <div className={styles.logo}>Hackathon</div>
+            <div className={`${styles.navLinks} ${open ? styles.open : ''}`}>
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+                <a href="/mentors">Mentors</a>
+                <a href="/contact">Contact</a>
+            </div>
+            <div className={styles.hamburger} onClick={() => setOpen(!open)}>
+                <span className={styles.bar}></span>
+                <span className={styles.bar}></span>
+                <span className={styles.bar}></span>
+            </div>
+        </nav>
     )
 }
 
